41028bf448b4b43317091b9cb94f651b6c105dfd
amesshaps <amesshaps@gmail.com>
2016-09-15 13:09:20 -0700

    Add ascii giraffe

--- exercise1.py
+++ exercise1.py
@@ -3,3 +3,18 @@ def print_message():
 
 if __name__ == '__main__':
     print_message()
+
+def print_art():
+    print("""\
+
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \
+                                  ,"  ## |   ಠ ಠ.
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+
+
+                    """)
